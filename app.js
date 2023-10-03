0fbe075a1a67d68fe9ed76ddd85f033e31940e37
Enkhbayr <enkhbayr049@gmail.com>
2023-10-04 00:23:15 +0800

    shoog shiddeg bolgoson

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // shoo ali talaara buusniig hadgalah huvsahch 1-6 gesen utga sanamsargui ogno
-var dice = Math.floor(Math.random() * 6) + 1;
-
 
 
 // program ehlehd boldeh
-document.querySelector("#score-0").textContent=0;
-document.querySelector("#score-1").textContent=0;
+document.getElementById("score-0").textContent=0;
+document.getElementById("score-1").textContent=0;
 
-document.querySelector("#current-0").textContent=0;
-document.querySelector("#current-1").textContent=0;
+document.getElementById("current-0").textContent=0;
+document.getElementById("current-1").textContent=0;
 
-document.querySelector(".dice").style.display = 'none';
-// document.querySelector("#score-0").textContent=dice;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = 'none';
+
+
+document.querySelector(".btn-roll").addEventListener("click", function shooshid() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = 'block';
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
