6c74f289bdfa38689062546b95c7989ca17952d4
Enkhbayr <enkhbayr049@gmail.com>
2023-10-05 18:11:45 +0800

    current onoog nemsen

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // toglogchiin eeljiig hadgalh huvsagch negugeer toglogch 0 hoyrdugaar toglogch 1
-var activePlayer = 1;
+var activePlayer = 0;
 
 // togldogch D giin togson onoog hadgalna
 var scores = [0, 0];
@@ -16,13 +16,34 @@ document.getElementById("score-1").textContent=0;
 
 document.getElementById("current-0").textContent=0;
 document.getElementById("current-1").textContent=0;
+// currunt onoog tegleh 
+
+
+// end teglle
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = 'none';
 
-
+// shoo shiddeg heseg event click heseg     
 document.querySelector(".btn-roll").addEventListener("click", function shooshid() {
+    // negees 6 hurtel sanamsargui too gargaj avna
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // buusan tooni zurgiig gargaj irne
     diceDom.style.display = 'block';
     diceDom.src = "dice-" + diceNumber + ".png";
+    if (diceNumber !== 1) {
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        // document.getElementById("current-0").textContent=0;
+        // roundScore = 0;
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        diceDom.style.display = 'none';
+    }
 });
+// toglogchiin eeljiin onoog oorchilno buusan too ni negees yalgaatai bol active toglogchiin onoog nemne
